bd61127260b24c6b56a182293e075e746f39bdfb
eunnu <angellina9413@gmail.com>
2023-02-06 10:25:20 +0900

    20230206 merge_01 key

---
+++ frontend/pjt01/src/addItemPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./css/addItem.css"
+
+function AddItemPage() {
+    return (
+      <div className="recipe">
+
+      </div>
+    );
+  }
+  
+  export default AddItemPage;
--- frontend/pjt01/src/components/ref/GetItem.js
+++ frontend/pjt01/src/components/ref/GetItem.js
@@ -5,7 +5,7 @@ import nambi from "../../img/nambi.png";
 function GetItem() {
     return (
       <div className='item'>
-        <img src={nambi} alt="nambi"></img>
+        <div className='ref_icon'><img src={nambi} alt="nambi" className='nambi_img' ></img></div>
         <div className='itemText'>추천받기</div>
       </div>
     );
