711d828c172f83d7e9a19ebcc27febfdf1936d41
jhyun3315 <jhyun3315@naver.com>
2023-02-10 21:14:26 +0900

    aws gradle 추가

--- backend/Project/bobs/src/main/java/com/b304/bobs/BobsApplication.java
+++ backend/Project/bobs/src/main/java/com/b304/bobs/BobsApplication.java
@@ -1,9 +1,7 @@
 package com.b304.bobs;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BobsApplication {
