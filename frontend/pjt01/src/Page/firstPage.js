555f52fc9affa69d68df0e3a1b516bdd2e0ba8a2
mayril <inho806901@gmail.com>
2023-02-12 16:56:19 +0900

    Update firstPage.js

--- frontend/pjt01/src/Page/firstPage.js
+++ frontend/pjt01/src/Page/firstPage.js
@@ -11,10 +11,12 @@ function FirstPage() {
   // 백엔드 리다이랙트 URI 예시
   // const REDIRECT_URI =  "http://localhost:5000/kakao/code";
   const history = useHistory();
+  const url = "https://i8b304.p.ssafy.io"
   const CLIENT_ID = "a170d137da8c6693eacb1d31f30d2d45";
-  const REDIRECT_URI =  "https://i8b304.p.ssafy.io/oauth2/authorization/kakao";
+  const REDIRECT_URI =  "/oauth2/authorization/kakao";
+  // const REDIRECT_URI =  "http://localhost:8080/oauth2/authorization/kakao"
   // const REDIRECT_URI =  "http://localhost:8080/oauth2/authorization/kakao";
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   const [check, setCheck] = useState(false)
   const [logincheck,setlogincheck] =useState(false);
   const fadeout = useRef();
@@ -25,18 +27,18 @@ function FirstPage() {
     const params=new URLSearchParams(location.search);
     var token=params.get('atk')
     var id=params.get("id");
+    
+
     if(token!==null){
       setaccesstoken(token.substring(7));
       sessionStorage.setItem("login", accesstoken);
     }
     const getlogin= sessionStorage.getItem("login");
-    console.log("login")
     if(getlogin){
-      console.log("login")
       setlogincheck(true);
       fadeout.current.id="complete"
     }
-    const url = "https://i8b304.p.ssafy.io"
+    
     // const url = "http://localhost:8080"
     console.log(sessionStorage.getItem("login"));
 
@@ -47,7 +49,6 @@ function FirstPage() {
         console.log(res.data.data.user_id)
         console.log(res.data.data.user_name)
         console.log(res.data.data.user_profile)
-        history.push("/");
       })
 
     // axios.get(url+'/api/users/find/'+sessionStorage.getItem("login"),
@@ -71,7 +72,7 @@ function FirstPage() {
   return (
     <div className='login_Page' ref={fadeout}>      
       {
-        check === false ? <></> : <a href={KAKAO_AUTH_URL}><button className="login_btn_block">로그인</button></a>  
+        check === false ? <></> : <a href={REDIRECT_URI}><button className="login_btn_block">로그인</button></a>  
       }   
       <div className='firset_logo_text'>Bobs</div>  
     </div>
