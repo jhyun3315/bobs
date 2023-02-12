a03a8daa1cef4859e6e7b2a81e34bb01592e8649
mayril <inho806901@gmail.com>
2023-02-12 18:49:54 +0900

    fix

--- frontend/pjt01/src/components/ref/AddItem.js
+++ frontend/pjt01/src/components/ref/AddItem.js
@@ -2,10 +2,30 @@ import React from 'react';
 import "./css/item.css"
 import plus from "../../img/plus.png";
 import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
 function AddItem() {
+  const [getUserItem,setgetUserItem] =useState([]);
 
   const history = useHistory()
+  useEffect(() => {
+    const url="https://i8b304.p.ssafy.io/api/refriges/user";
+    axios.post(url,{
+      
+    })
+      .then(function(response) {
+        setgetUserItem(response.data);
+        console.log("성공");
+    })
+      .catch(function(error) {
+          console.log("실패");
+    })
+  
+    
+  }, [])
+  
 
   function onClick() {
     history.push('/refridgerator/add')
