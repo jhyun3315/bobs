6123ed5cd5887603c6e351b22f5017ba2085c665
jhyun3315 <jhyun3315@naver.com>
2023-02-14 00:12:59 +0900

    스터디 댓글 수정

--- backend/Project/bobs/src/main/java/com/b304/bobs/api/service/StudyCommentService.java
+++ backend/Project/bobs/src/main/java/com/b304/bobs/api/service/StudyCommentService.java
@@ -1,5 +1,7 @@
 package com.b304.bobs.api.service;
 
+import com.b304.bobs.api.request.StudyCommentModifyReq;
+import com.b304.bobs.api.request.StudyCommentReq;
 import com.b304.bobs.api.response.ModifyRes;
 import com.b304.bobs.api.response.PageRes;
 import com.b304.bobs.api.response.StudyCommentRes;
@@ -7,8 +9,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface StudyCommentService {
-    public StudyCommentRes createComment(StudyCommentRes studyCommentRes) throws Exception;
-    public ModifyRes deleteComment(Long study_id) throws Exception;
-    public ModifyRes modifyComment(StudyCommentRes studyCommentRes) throws Exception;
-    public PageRes findAll(Long comment_id) throws Exception;
+    StudyCommentRes createComment(StudyCommentReq studyCommentReq) throws Exception;
+    ModifyRes deleteComment(Long study_id) throws Exception;
+    ModifyRes modifyComment(StudyCommentModifyReq studyCommentModifyReq) throws Exception;
+    PageRes findAll(Long comment_id) throws Exception;
+    StudyCommentRes findById(Long study_comment_id) throws Exception;
 }
