1bd7b70ba82582c4d95246892b7cdc01d2604819
be_seungmin <ityesmean@naver.com>
2023-02-14 10:11:34 +0900

    0214 - study member 가입

--- backend/Project/bobs/src/main/java/com/b304/bobs/api/service/StudyMemberService.java
+++ backend/Project/bobs/src/main/java/com/b304/bobs/api/service/StudyMemberService.java
@@ -2,10 +2,14 @@ package com.b304.bobs.api.service;
 
 import com.b304.bobs.api.request.StudyMemberReq;
 import com.b304.bobs.api.response.ModifyRes;
+import com.b304.bobs.api.response.StudyMemberRes;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface StudyMemberService {
+
+    // 가입
+    StudyMemberRes createStudyMember(StudyMemberReq studyMemberReq) throws Exception;
 
     // 구성원 수 조회
     Long countMember(Long studyId) throws Exception;
